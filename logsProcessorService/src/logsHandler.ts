f110241bd0fcc0e8683b7fec81c1fea44d244efd
benamsalem <benamsalem6@gmail.com>
2025-04-29 16:43:20 +0300

    write logs to file

--- logsProcessorService/src/logsHandler.ts
+++ logsProcessorService/src/logsHandler.ts
@@ -1,15 +1,50 @@
 // @ts-ignore
 import {LOGS_QUEUE_ID, NEW_MESSAGE_EVENT} from "./consts/queues.ts";
 // @ts-ignore
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+// @ts-ignore
 import {getFromQueue} from "./mocks/queue.ts";
 // @ts-ignore
 import {emitter} from "./mocks/events.ts";
 
+// @ts-ignore
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//todo separate to time frame files
+const logsDir = '../logs';
+const logsFileName = 'logFile.txt'
+const dirPath = join(__dirname, logsDir);
+const filePath = join(dirPath, logsFileName);
+
+async function fileExists(filePath) {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+async function writeLog(logAsString: string) {
+    const isExist = await fileExists(filePath)
+    if (!isExist) {
+        await fs.mkdir(dirPath, { recursive: true });
+        await fs.writeFile(filePath, logAsString + '\n');
+        return;
+    }
+    await fs.appendFile(filePath, logAsString + '\n');
+
+}
+
 emitter.on(NEW_MESSAGE_EVENT + LOGS_QUEUE_ID, () => {
-    const log = JSON.parse(getFromQueue(LOGS_QUEUE_ID));
-    console.log('new log logs handler', log);
+    const logAsString = getFromQueue(LOGS_QUEUE_ID)
+    const log = JSON.parse(logAsString);
+    console.log('new log logsHandler', log);
     // todo process the log
-    // todo store the log
+    writeLog(logAsString);
     // todo send alert if necessary
 })
 export const a = 1;
